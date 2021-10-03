b977e84b5893b53352cd3050cff7302e1d695791
houcze <houchengze@republicofhou.cn>
2021-10-03 16:25:40 +0800

    Update get-houcz-conda.sh

--- get-houcz-conda.sh
+++ get-houcz-conda.sh
@@ -1,8 +1,10 @@
 git clone https://github.com/houcze/hconda.git ~/.hconda
 cd ~/.hconda
-echo "export PATH=\$HOME/.hconda:$PATH" > ~/.bashrc
+echo "export PATH=\$HOME/.hconda:$PATH" >> ~/.bashrc
+echo "source ~/.hconda/profile" >> ~/.bashrc
+echo "alias conda=\"source conda\"" >> ~/.bashrc
 chmod 755 $(which conda)
 mkdir /opt/package
 mkdir /opt/env
 chown -R -v $(whoami):$(whoami) /opt/package /opt/env
-rm ~/.hconda/get-houcz-conda.sh
+rm $HOME/.hconda/get-houcz-conda.sh
